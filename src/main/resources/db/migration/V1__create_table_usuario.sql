927e6bb10e32205bd10c5e774bb1e240102bc0ae
OtavioFicagna <oaugusto796@gmail.com>
2023-10-15 21:30:39 -0300

    Final true

--- src/main/resources/db/migration/V1__create_table_usuario.sql
+++ src/main/resources/db/migration/V1__create_table_usuario.sql
@@ -2,5 +2,8 @@ CREATE TABLE `usuario` (
  `id` bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
  `cidade` varchar(255) DEFAULT NULL,
  `nome` varchar(255) NOT NULL,
- `telefone` varchar(255) DEFAULT NULL
+ `telefone` varchar(255) DEFAULT NULL,
+ `role` enum("USER", "ADMIN") DEFAULT "USER",
+ `email` varchar(255) NOT NULL UNIQUE,
+ `senha` varchar(255) NOT NULL
 );
